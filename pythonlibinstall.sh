64bc9f4c1f315d949eaed6b3d4a966f85fa38562
Screen <screenthong.l@g-able.com>
2017-11-24 08:27:06 +0700

    Update pythonlibinstall.sh

--- pythonlibinstall.sh
+++ pythonlibinstall.sh
@@ -5,3 +5,8 @@
 
 # upgrade numpy version
 /usr/bin/anaconda/bin/conda update numpy
+
+# link python to /usr/bin/anaconda/
+cd /usr/bin
+rm python
+ln -s /usr/bin/anaconda/bin/python2.7 /usr/bin/python
